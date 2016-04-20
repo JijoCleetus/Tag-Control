993bcf8eb0bcb816f7cc21c3203dfedde792dd0d
Jijo Cleetus <jijocleetus@gmail.com>
2016-04-20 19:22:00 +0530

    Added files via upload

---
+++ App/app.js
@@ -0,0 +1,15 @@
+var app = angular.module("myApp", []);
+app.controller("myController", function ($scope) {
+    $scope.mytags = [];
+    
+    $scope.addTag = function (keyevent) {
+        if (keyevent.which === 13) {
+            $scope.mytags.push($scope.tag);
+            $scope.tag = "";
+        }
+  }
+    $scope.cleartag=function()
+    {
+
+    }
+});
